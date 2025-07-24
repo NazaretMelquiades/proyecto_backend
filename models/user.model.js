0252603812a01ad9603a5abfc44c8edd21a533fc
Miguel <migueljm1008@gmail.com>
2025-07-24 19:44:16 +0200

    fixed/films & user models

--- models/user.model.js
+++ models/user.model.js
@@ -1,110 +1,40 @@
 const pool = require('../db');
 const queries = require('./queries/user.queries');
+const { executeQuery } = require('../utils/pgHelper');
 
 const signUpUser = async (user) => {
   const { username, email, password } = user;
-  let client;
-  try {
-    client = await pool.connect();
-    const result = await client.query(queries.signUpUser, [username, email, password]);
-    return result.rowCount;
-  } catch (err) {
-    console.error('Error en signUpUser:', err);
-    throw err;
-  } finally {
-    client.release();
-  }
-};
-
+  return await executeQuery(queries.signUpUser, [username, email, password]);
+}
 
 const getUserByEmail = async (email) => {
-  let client;
-  try {
-    client = await pool.connect();
-    const result = await client.query(queries.getUserByEmail, [email]);
-    return result.rows[0]; // retorna el usuario o undefined
-  } catch (err) {
-    console.error('Error en getUserByEmail:', err);
-    throw err;
-  } finally {
-    client.release();
-  }
-};
+  return await executeQuery(queries.getUserByEmail, [email]);
+}
 
 // Obtener todos los usuarios
 const getAllUsers = async () => {
-  let client;
-  try {
-    client = await pool.connect();
-    const result = await client.query(queries.getAllUser);
-    return result.rows;
-  } catch (err) {
-    console.error('Error en getAllUsers:', err);
-    throw err;
-  } finally {
-    client.release();
-  }
+  return await executeQuery(queries.getAllUser);
 };
 
 // Actualizar un usuario
 const updateUser = async (user) => {
   const { username, email, password, id } = user;
-  let client;
-  try {
-    client = await pool.connect();
-    const result = await client.query(queries.updateUser, [username, email, password, id]);
-    return result.rowCount;
-  } catch (err) {
-    console.error('Error en updateUser:', err);
-    throw err;
-  } finally {
-    client.release();
-  }
+  return await executeQuery(queries.updateUser, [username, email, password, id]);
 };
 
 // Eliminar usuario por email
 const deleteUser = async (email) => {
-  let client;
-  try {
-    client = await pool.connect();
-    const result = await client.query(queries.deleteUser, [email]);
-    return result.rowCount;
-  } catch (err) {
-    console.error('Error en deleteUser:', err);
-    throw err;
-  } finally {
-    client.release();
-  }
+  return await executeQuery(queries.deleteUser, [email]);
 };
 
 // Marcar usuario como logueado
 const logIn = async (email) => {
-  let client;
-  try {
-    client = await pool.connect();
-    const result = await client.query(queries.logIn, [email]);
-    return result.rowCount;
-  } catch (err) {
-    console.error('Error en logIn:', err);
-    throw err;
-  } finally {
-    client.release();
-  }
+  return await executeQuery(queries.logIn, [email]);
 };
 
 // Marcar usuario como deslogueado
 const logOut = async (email) => {
-  let client;
-  try {
-    client = await pool.connect();
-    const result = await client.query(queries.logOut, [email]);
-    return result.rowCount;
-  } catch (err) {
-    console.error('Error en logOut:', err);
-    throw err;
-  } finally {
-    client.release();
-  }
+  return await executeQuery(queries.logOut, [email]);
 };
 
 module.exports = {

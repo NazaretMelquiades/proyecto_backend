27bd3c8f5db75775a10720e2d88bf7e3fbff2ee4
choski91 <mlaura.smichowski@gmail.com>
2025-07-29 18:03:12 +0200

    fixed/userRoutes in app.js

--- app.js
+++ app.js
@@ -16,8 +16,8 @@ const morgan = require('./middlewares/morgan');
 app.set('view engine', 'pug');
 app.set('views', './views');
 
-// Middleware para servir archivos estáticos (como CSS)
-app.use(express.static('public'));
+// // Middleware para servir archivos estáticos (como CSS)
+// app.use(express.static('public'));
 
 // Configuración del logger con morgan
 app.use(morgan(':method :url :status :param[id] - :response-time ms :body'));
@@ -33,7 +33,7 @@ const filmsRoutes = require('./routes/films.route');
 
 //agrgado 28/07 rutas de paginas
 const pagesRoutes = require('./routes/pages.route');
-const userRoutes = require('./routes/user.routes');
+const userRoutes = require('./routes/user.route');
 const favsRoutes = require('./routes/favs.routes');
 
 
@@ -41,9 +41,9 @@ app.use(express.json());
 
 // Rutas
 //API
-// app.use('/api/films', filmsRoutes);
+app.use('/api/films', filmsRoutes);
 app.use('/api', userRoutes);
-// app.use('/api/favorites', favsRoutes);
+app.use('/api/favorites', favsRoutes);
 app.use('/', pagesRoutes);
 
 // Gestionar ruta inexistente
